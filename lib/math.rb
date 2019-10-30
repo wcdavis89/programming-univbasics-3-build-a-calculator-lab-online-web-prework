4be447aa740d2326d622e2bf8e9cdd854c1c20e9
Whittney Davis <wcdavis89@yahoo.com>
2019-10-30 16:57:39 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+sum=num1+num2
+sum
 end
 
 def subtraction(num1, num2)
-
+dif=num1-num2
+dif
 end
 
 def division(num1, num2)
-
+pro=num1/num2
+pro
 end
 
 def multiplication(num1, num2)
-
+quo=num1*num2
+quo
 end
 
 def modulo(num1, num2)
-
+rem=num1%num2
+rem
 end
 
 def square_root(num)
-
+result=Math.sqrt(num)
+result
 end
