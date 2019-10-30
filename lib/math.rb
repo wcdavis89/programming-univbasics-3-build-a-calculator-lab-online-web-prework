2002de815ac79b4660be5d0bd8c930f7da31e243
Whittney Davis <wcdavis89@yahoo.com>
2019-10-30 16:53:05 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,21 @@
 def addition(num1, num2)
 sum=num1+num2
+sum
 end
 
 def subtraction(num1, num2)
-
+dif=num1-num2
+dif
 end
 
 def division(num1, num2)
-
+pro=num1*num2
+pro
 end
 
 def multiplication(num1, num2)
-
+quo=num1*num2
+quo
 end
 
 def modulo(num1, num2)
